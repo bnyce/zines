ef5df65c6e4e72f5574088a4f662de5a883e5500
Bryce Benton <brycebenton+pantheon2@gmail.com>
2022-12-28 19:49:04 -0600

    checky

--- html/p5js/checkerboard/sketch.js
+++ html/p5js/checkerboard/sketch.js
@@ -1,49 +1,33 @@
 function setup() {
-	frameRate(2);
-	i = 0;
-	winX = 500;
-	winY = 500;
-	createCanvas(winX, winY);
-	background(0);
-	
-	grid = 10;
-	check = winX / grid; // 500 / 10 = 50
-	bigcheck = check * 3;
-	
-}
-function grid_a(x = 0,y = 0) {
-	rect(x,y,check,check);
-	ellipse(x,y,check,check);
-}
-function grid_b(x = 0, y = 0){
-push();
-translate(x,y);
-grid_a(0,0);
-fill('red'); // or fill(255,0,0) or fill(255,0,0,255)
-grid_a(check * 2,0);
-fill('green');
-grid_a(check,check);
-fill('blue');
-grid_a(0,check * 2);
-fill(0,0,255,102); //RGB, alpha (by default on scale to 255, but can be normalized to max 100)
-grid_a(check * 2, check * 2);
-pop();
-}
-function grid_c(x = 0, y = 0) {
+  ch_color = 'white'
+  grid_size = 7
 
-grid_b();
-grid_b(bigcheck * 2);	
-grid_b(bigcheck,bigcheck);
-grid_b(0,bigcheck * 2);
-grid_b(bigcheck * 2,bigcheck * 2);
-pop();
+
+  winX = 500
+  winY = 500
+  createCanvas(winX, winY)
+  ch = winX / grid_size
+  fill(ch_color)
 }
+
+function checky(x = 0, y = 0) {
+  rect(x, y, ch, ch)
+}
+
+function grid_b(grid_size, x = 0, y = 0) {
+  push()
+  translate(x, y)
+  for (i = 0; i < grid_size; i++) {
+    checky(ch * i, ch * i)
+  }
+  pop()
+}
+
 function draw() {
-//noLoop();
-grid_c();
-i++;
-if(i > 10) {
-	clear();
-	i = 0;
-}
+  noLoop()
+  grid_b(grid_size, ch * 0)
+  for (j = 0; j < grid_size; j = j + 2) {
+    grid_b(grid_size, ch * j)
+    grid_b(grid_size, ch * -j)
+  }
 }
