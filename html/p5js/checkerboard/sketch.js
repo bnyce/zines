47dd9e342f3bb467140bff7a5e24d822664f9365
Bryce Benton <brycebenton+pantheon2@gmail.com>
2022-12-28 19:56:17 -0600

    cleanup

--- html/p5js/checkerboard/sketch.js
+++ html/p5js/checkerboard/sketch.js
@@ -1,33 +1,28 @@
 function setup() {
-  ch_color = 'white'
-  grid_size = 7
-
-
-  winX = 500
-  winY = 500
-  createCanvas(winX, winY)
-  ch = winX / grid_size
-  fill(ch_color)
+  rows = 5
+  size = 500
+  fill('white')
+  createCanvas(size, size)
+  ch = size / rows
 }
 
-function checky(x = 0, y = 0) {
+function chk(x = 0, y = 0) {
   rect(x, y, ch, ch)
 }
 
-function grid_b(grid_size, x = 0, y = 0) {
+function chex(rows, x = 0, y = 0) {
   push()
   translate(x, y)
-  for (i = 0; i < grid_size; i++) {
-    checky(ch * i, ch * i)
+  for (i = 0; i < rows; i++) {
+    chk(ch * i, ch * i)
   }
   pop()
 }
 
 function draw() {
   noLoop()
-  grid_b(grid_size, ch * 0)
-  for (j = 0; j < grid_size; j = j + 2) {
-    grid_b(grid_size, ch * j)
-    grid_b(grid_size, ch * -j)
+  for (j = 1; j < rows; j = j + 2) {
+    chex(rows, ch * j)
+    chex(rows, ch * -j)
   }
 }
